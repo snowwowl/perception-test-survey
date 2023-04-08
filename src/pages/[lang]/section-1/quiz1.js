3dcabbfe2caa2d7bfa2ae21dafcca5262c0c1a9c
Ayush Choudhury <ayush.choudhury@iiitg.ac.in>
2023-04-08 12:00:42 +0530

    Update quiz1.js

--- src/pages/[lang]/section-1/quiz1.js
+++ src/pages/[lang]/section-1/quiz1.js
@@ -10,11 +10,12 @@ import {
 } from '@chakra-ui/react';
 import data from '@/data/section1/quiz.json';
 import { Elsie, Poppins } from 'next/font/google';
+import AudioButton from '@/components/AudioButton';
 
 const elsie = Elsie({ weight: '900', subsets: ['latin'] });
 const poppins = Poppins({ weight: '400', subsets: ['latin'] });
 
-export default function Quiz({pageData}) {
+export default function Quiz({ pageData }) {
     return (
         <>
             <Box
@@ -29,9 +30,31 @@ export default function Quiz({pageData}) {
                         </Text>
                     )
                 })}
+                <Box></Box>
+                <Box py={16}>
+                    <Flex
+                        justifyContent='space-evenly'
+                        rowGap={2}
+                        columnGap={1}
+                        flexDirection={'row'}
+                        flexWrap={'wrap'}
+                        flexBasis={'100%'}>
+                        {[...Array(20)].map((el, idx) => <AudioButton key={idx} idx={idx + 1} />)}
+                    </Flex>
+                </Box>
 
-                <Box>
-                    
+                <Box position='absolute' right='16' pt={8} pb={4}>
+                    <Button
+                        _hover={{ color: 'black' }}
+                        size='lg'
+                        rounded={32}
+                        variant='solid'
+                        color='#F5E3E3'
+                        backgroundColor='#5151D2'
+
+                    >
+                        Next
+                    </Button>
                 </Box>
             </Box>
 
